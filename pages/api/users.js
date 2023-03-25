24d0a8d3a809b83c9e9283467c116252c8573c4f
Richu Sony <sonyrichu4@gmail.com>
2023-03-25 16:34:38 +0530

    few changes

--- pages/api/users.js
+++ pages/api/users.js
@@ -8,5 +8,6 @@ export default async function handler(req, res) {
     Authorization: `Bearer ${accessToken}`,
   };
   const { data } = await axios.get(url, { headers });
+ console.log(data);
   res.status(200).json(data);
 }
