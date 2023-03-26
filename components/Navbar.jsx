bd08ff2c1fb916b9e526b97e868108afec34cf66
Richu Sony <sonyrichu4@gmail.com>
2023-03-26 14:30:08 +0530

    login changed to normal

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
                         <li className="text-xl p-3 md:p-0 md:mx-auto w-[100px] border-b-2 border-black md:border-none select-none ease-linear"><Link href="/donation" className="transition duration-300 ease-linear hover:font-medium hover:italic">Donation</Link></li>
                         <li className="text-xl p-3 md:p-0 md:mx-auto w-[100px] border-b-2 border-black md:border-none cursor-pointer select-none ease-linear hover:font-medium hover:italic" onClick={scrollToFoot}>Contact</li>
                         <li className="text-xl p-3 md:p-0 md:mx-auto w-[100px] border-b-2 border-black md:border-none cursor-pointer select-none ease-linear hover:font-medium hover:italic" onClick={scrollToFoot}>FAQ</li>
-                        {/* <h3 id='new' className="transition duration-150 text-xl p-3 md:p-0 md:mx-auto w-[100px] md:border-none select-none md:rounded md:bg-[#151522] md:text-white cursor-pointer ease-in hover:scale-95" onClick={handleLogin}>Login</h3> */}
+                        <h3 id='new' className="transition duration-150 text-xl p-3 md:p-0 md:mx-auto w-[100px] md:border-none select-none md:rounded md:bg-[#151522] md:text-white cursor-pointer ease-in hover:scale-95"><Link href="/Login">Login</Link></h3>
                     </ul>
                 </div>
             </>
