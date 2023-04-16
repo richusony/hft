d6a6d0f092e3270928c0b79c34836fd6e535fb98
Richu Sony <sonyrichu4@gmail.com>
2023-04-16 17:27:21 +0530

    added reset password

--- pages/Login/reset.js
+++ pages/Login/reset.js
@@ -74,7 +74,7 @@ const reset = () => {
                 />
                 <div className="flex-row w-72 h-80 text-center mx-auto border px-3 mt-10">
                     <h2 className="text-xl w-fit text-center mx-auto font-medium text-white mt-6 mb-5">Enter the Email</h2>
-                    <input type="tel" placeholder="email" className="block w-fit mx-auto placeholder:p-3 rounded h-10 mb-3 bg-inherit border text-white" required onChange={(e) => setEmail(e.target.value)} />
+                    <input type="email" placeholder="email" className="block w-fit mx-auto placeholder:p-3 rounded h-10 mb-3 bg-inherit border text-white" required onChange={(e) => setEmail(e.target.value)} />
                     <button className="block mx-auto border p-2 text-2xl text-white rounded w-36 my-4" onClick={(e) => { sendMail(e) }}>send</button>
                 </div>
                 <div className="p-1 mt-20">
