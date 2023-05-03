4e556819a59da1cbeacbbe347966eba384f7a0c7
Richu Sony <sonyrichu4@gmail.com>
2023-05-03 23:53:04 +0530

    few changes

--- pages/create-account/index.js
+++ pages/create-account/index.js
@@ -34,6 +34,18 @@ const create_account = () => {
             const validEmail = email.match(
                 /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
             );
+            if(!name && !email && !password){
+                toast.warning('Fill all Details!', {
+                    position: "bottom-left",
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                });
+            }
 
             if (password) {
                 if (password.length < 8) {
